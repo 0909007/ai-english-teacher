a306e0a2de555ef3b2ae72696065972bca1ded91
0909007 <0909007@gmail.com>
2025-05-16 12:51:07 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-    port: 5173,
-    open: true,
-  },
+// vite.config.js
+export default {
+  base: './',
   build: {
     outDir: 'dist',
   },
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
-})
+};
