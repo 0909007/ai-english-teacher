539cff8c8cb82dca96b5a8577cab02862d19e537
0909007 <0909007@gmail.com>
2025-05-16 12:57:22 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-// vite.config.js
-export default {
-  base: './',
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: './',  // 상대경로로 빌드해서 경로 문제 방지
   build: {
-    outDir: 'dist',
+    outDir: 'dist',  // 빌드 산출물 폴더 지정
   },
-};
+})
